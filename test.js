a7af211056d8a97e3e3cf4988383da361b6507bf
Muhd Danial <mdanial1410@outlook.com>
2021-03-05 13:23:09 +0800

    2nd phase testing. almost there

--- test.js
+++ test.js
@@ -3,37 +3,26 @@ const axios = require("axios");
 let api_url =
 	"https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login";
 
-async function login() {
-	// return await axios({
-	// 	method: "post",
-	// 	url: api_url,
-	// 	headers: {
-	// 		"x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40",
-	// 	},
-	// 	data: {
-	// 		username: "Group12",
-	// 		password: "Asu_MHFWvDbsXKE",
-	// 	},
-	// }).then(function (response) {
-	// 	return response.data;
-	// });
+async function login(username, password) {
+	const config = {
+		method: "post",
+		url: api_url,
+		headers: {
+			"x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40",
+		},
+		data: {
+			username: username,
+			password: password,
+		},
+	};
 
-	return axios
-		.post(api_url, {
-			headers: {
-				"x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40",
-			},
-			params: {
-				username: "Group12",
-				password: "Asu_MHFWvDbsXKE",
-			},
-		})
-		.then(function (response) {
-			return response.data;
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+	let res = await axios(config);
+	return res.data;
 }
 
-console.log(login());
+let username = "Group12";
+let password = "Asu_MHFWvDbsXKE";
+
+login(username, password).then((data) => {
+	console.log(data);
+});
