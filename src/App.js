930987bac5d8577290b66e57313e8711fc05c240
lexades <jianxiangpang@gmail.com>
2021-03-05 13:05:22 +0800

    simple router test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Login from './Login/Login'
+import Dashboard from './Dashboard/Dashboard'
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
 
 function App() {
   return (
     <div>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login}/>
+          <Route path="/dashboard" component={Dashboard}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
