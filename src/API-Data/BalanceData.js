f501ada568f48504062f8ecd306f3062a1c7244b
MarcusTsY <marcustsy@outlook.com>
2021-03-05 13:33:00 +0800

    Balance Data

---
+++ src/API-Data/BalanceData.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+balanceData = {};
+let api_url =
+	"https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance";
+
+let a = axios({
+	method: "post",
+	url: api_url,
+	headers: { "x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40" },
+	data: {
+		accountKey: "e200e4e0-e45a-4923-9079-6ebd929f97db",
+	},
+});
+
+a.then((data) => {
+	balanceData = data["data"];
+    console.log(balanceData);
+});
+
+export default balanceData;
+
