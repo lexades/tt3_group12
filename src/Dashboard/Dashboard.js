a16ed4537e309da1eefbd4e3574f2630b02468be
lexades <jianxiangpang@gmail.com>
2021-03-05 13:04:10 +0800

    dashboard static

---
+++ src/Dashboard/Dashboard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {BrowserRouter as Router,Route,Switch,Link} from "react-router-dom"
+
+import './Dashboard.styles.css'
+
+function Dashboard(){
+    return(
+        <div className='dashbox'>
+            <h1 className='welcome'>Welcome back, user.name!</h1>
+            <marquee>Have a pleasant banking experience with DBS!</marquee>
+            <div>
+                <table>
+                    <tr>
+                        <td><button>Balance</button></td>
+                        <td><button>Transaction</button></td>
+                        <td><button>Market</button></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
