9655f8dbb4366d9441604ba970409e4bacceff4a
Muhd Danial <mdanial1410@outlook.com>
2021-03-05 13:03:36 +0800

    still testing

--- test.js
+++ test.js
@@ -1,21 +1,39 @@
 const axios = require("axios");
-let temp;
 
 let api_url =
 	"https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login";
 
-let a = axios({
-	method: "post",
-	url: api_url,
-	headers: { "x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40" },
-	data: {
-		username: "Group12",
-		password: "Asu_MHFWvDbsXKE",
-	},
-});
+async function login() {
+	// return await axios({
+	// 	method: "post",
+	// 	url: api_url,
+	// 	headers: {
+	// 		"x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40",
+	// 	},
+	// 	data: {
+	// 		username: "Group12",
+	// 		password: "Asu_MHFWvDbsXKE",
+	// 	},
+	// }).then(function (response) {
+	// 	return response.data;
+	// });
 
-a.then((data) => {
-	temp = data["data"];
-});
+	return axios
+		.post(api_url, {
+			headers: {
+				"x-api-key": "BDTv5isstyQr8aGI2xNp5CzqSYFCrtXPIAZV4Z40",
+			},
+			params: {
+				username: "Group12",
+				password: "Asu_MHFWvDbsXKE",
+			},
+		})
+		.then(function (response) {
+			return response.data;
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
 
-console.log(temp[0]);
+console.log(login());
