6c6076e682400881939be8687d0871089176676f
lexades <jianxiangpang@gmail.com>
2021-03-05 14:32:31 +0800

    get response from pricing, spoch data converted

--- src/Pricing/Pricing.js
+++ src/Pricing/Pricing.js
@@ -2,9 +2,13 @@ import React,{useState} from 'react';
 import Axios from 'axios';
 import {useHistory} from 'react-router-dom';
 
-import './Login.styles.css'
+
 
 export default function Pricing(){
+
+    const [assetSymbol,setassetSymbol]=useState('')
+    const [price,setprice]=useState('')
+    const [timeStamp,setimeStamp]=useState('')
 
     let Config = {
         headers: {
@@ -13,19 +17,29 @@ export default function Pricing(){
     };
 
     const PricingList = () => {
-        Axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login', [], Config)
+        Axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/historical',{}, Config)
         .then((response) => {
-            console.log(response);
+
+            //convert timestamp epoch datatype
+            var utcSeconds = response.data[0].timestamp;
+            var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+            d.setUTCSeconds(utcSeconds);//set to readable timestamp
+            console.log(d);
+
+            console.log(response.data[0].timestamp);
             }
         );
     };
+
+    PricingList();
+
 
     return(
         <div>
             <table>
             <tbody>
                 <tr>
-                    <td></td>
+                    <td>hello</td>
                 </tr>
             </tbody>
             </table>
