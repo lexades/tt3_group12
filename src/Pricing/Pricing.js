2fc858729d0a3b63fbeb17bfd993149672a605e3
lexades <jianxiangpang@gmail.com>
2021-03-05 15:39:11 +0800

    pricingtable

--- src/Pricing/Pricing.js
+++ src/Pricing/Pricing.js
@@ -2,13 +2,15 @@ import React,{useState} from 'react';
 import Axios from 'axios';
 import {useHistory} from 'react-router-dom';
 
+import './Pricing.styles.css'
+
 
 
 export default function Pricing(){
 
     const [assetSymbol,setassetSymbol]=useState('')
     const [price,setprice]=useState('')
-    const [timeStamp,setimeStamp]=useState(Date())
+    const [timeStamp,setimeStamp]=useState('')
 
     let Config = {
         headers: {
@@ -16,10 +18,10 @@ export default function Pricing(){
         }
     };
 
+
     const PricingList = () => {
         Axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/historical',{}, Config)
         .then((response) => {
-           
             //convert timestamp epoch datatype
             var utcSeconds = response.data[0].timestamp;
             var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
@@ -27,9 +29,12 @@ export default function Pricing(){
             console.log(d);
             var datee=d.toLocaleDateString()
 
-            setassetSymbol(response.data[0].assetSymbol);
-            setprice(response.data[0].price);
-            setimeStamp(datee);
+                //if select = price 
+                //show prices
+                setassetSymbol(response.data[0].assetSymbol);
+                setprice(response.data[0].price);
+                setimeStamp(datee);
+
             }
         );
     };
@@ -39,13 +44,21 @@ export default function Pricing(){
 
     return(
         <div>
-            <table>
+            <table className='pricetable'>
             <tbody>
                 <tr>
                     <td>Asset Symbol</td>
                     <td>{assetSymbol}</td>
+                    <td>Price</td>
                     <td>{price}</td>
+                    <td>TimeStamp</td>
                     <td>{timeStamp}</td>
+                </tr>
+                <tr>
+                <td>
+                    <input type='number' placeholder='Price'></input>
+                    <input type='date'></input>
+                    </td>
                 </tr>
             </tbody>
             </table>
